3923a0d26f9c6923292d7177cbe55bf9e367fa7e
Hugo Lima <hmiguellima@gmail.com>
2017-12-10 01:00:03 +0000

    Initial commit

---
+++ SteamLauncher/pages/game/game.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    "use strict";
+
+    var ControlConstructor = WinJS.UI.Pages.define("/pages/game/game.html", {
+        ready: function (element, options) {
+            var self = this;
+            WinJS.xhr({
+                url: 'http://store.steampowered.com/api/appdetails?appids=' + options.gameId,
+                responseType: 'json'
+            }).done(
+                function completed(result) {
+                    if (result.status === 200) {
+                        self.game = result.response[options.gameId].data;
+                        self._data = WinJS.Binding.as(self.game);
+                        WinJS.Binding.processAll(element, self._data).then(function () {
+                            WinJS.Utilities.query('#playButton').get(0).addEventListener('click', function () {
+                                var uri = new Windows.Foundation.Uri('steam://run/' + options.gameId);
+                                Windows.System.Launcher.launchUriAsync(uri);
+                            });
+                        });
+                    }
+                });
+        }
+    });
+
+    WinJS.Namespace.define('Controls_PageControls', {
+        GamePageControl: ControlConstructor
+    });
+})();
---
+++ SteamLauncher/pages/home/home.js
@@ -0,0 +1,62 @@
+﻿(function () {
+    "use strict";
+
+    var nav = WinJS.Navigation;
+    var gameList = new WinJS.Binding.List();
+
+    WinJS.Namespace.define('SteamLauncher.gameList', {
+        data: gameList
+    });
+
+    var ControlConstructor = WinJS.UI.Pages.define("/pages/home/home.html", {
+        ready: function (element, options) {
+            var self = this;
+            this._data = WinJS.Binding.as(options);
+
+            gameList.splice(0, gameList.length);
+            WinJS.Binding.processAll(element, this._data).then(function () {
+                var listView = WinJS.Utilities.query('#listView')[0].winControl;
+                listView.addEventListener("iteminvoked", function (evt) {
+                    var game = gameList.getAt(evt.detail.itemIndex)
+                    nav.navigate('/pages/game/game.html', game);
+                });
+                self.loadData();
+            });
+        },
+
+        loadData: function() {
+            WinJS.xhr({
+                url: "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?include_appinfo=1&key=" + SteamLauncher.key + "&steamid=" + SteamLauncher.profile + "&format=json",
+                responseType: "json"
+            }).done(
+                function completed(result) {
+                    var i;
+                    var games;
+                    var gameId;
+                    var title;
+                    var imgHash;
+
+                    if (result.status === 200) {
+                        games = result.response.response.games.sort(function (a, b) {
+                            return a.playtime_2weeks ? b.playtime_2weeks ? 0 : -1 : b.playtime_2weeks ? 1 : 0;
+                        });
+                        for (i = 0; i < games.length; i++) {
+                            gameId = games[i].appid;
+                            title = games[i].name;
+                            imgHash = games[i].img_logo_url;
+                            gameList.push({
+                                runUrl: "steam://run/" + gameId,
+                                gameId: gameId,
+                                title: title,
+                                picture: "http://media.steampowered.com/steamcommunity/public/images/apps/" + gameId + "/" + imgHash + ".jpg"
+                            });
+                        }
+                    }
+                });
+        }
+    });
+
+    WinJS.Namespace.define('Controls_PageControls', {
+        HomePageControl: ControlConstructor
+    });
+})();
